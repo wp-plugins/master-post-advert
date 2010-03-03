4d0fe54a37d44a1047b1b5422356d03a84d0890b
m_i_n <m_i_n@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-03 09:43:55 +0000

    Dodane pliki aktualnej wersji.
    git-svn-id: https://plugins.svn.wordpress.org/master-post-advert/trunk@212478 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ options.php
@@ -0,0 +1,26 @@
+<div class="wrap">
+  <h2><?php _e('Master Post Advert Settings', $this->name); ?></h2>
+  <p><?php _e('Settings of Your ad', $this->name); ?>.</p>
+  <form method="post" action="options.php">
+    <?php settings_fields($this->name.'_options'); ?>
+    <?php $options = $this->get_options(); ?>
+    <table class="form-table">
+      <tr valign="top">
+        <th scope="row"><?php _e('Advert area title', $this->name); ?>:</th>
+        <td><input type="text" name="master_post_advert[title]" value="<?php echo htmlspecialchars($options['title']); ?>" class="regular-text code" /> <span class="description"><?php _e('HTML enabled', $this->name); ?></span></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row"><?php _e('Advert area width', $this->name); ?>:</th>
+        <td><input type="text" name="master_post_advert[width]" value="<?php echo $options['width']; ?>" class="small-text" /> <span class="description">px, 0 = <?php _e('automatic', $this->name); ?></span></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row"><?php _e('Advert code', $this->name); ?>:</th>
+        <td><textarea name="master_post_advert[code]" cols="50" rows="10" class="large-text code"><?php echo htmlspecialchars($options['code']); ?></textarea></td>
+      </tr>
+    </table>
+    <p class="submit">
+      <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+  </form>
+  <p>Copyright &copy;2010 <a href="http://www.bbproject.net">BBProject.net</a></p>
+</div>
---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Master Post Advert ===
+Contributors: m_i_n
+Donate link: http://www.bbproject.net
+Tags: advert, advertisement, ad, post, adsense, tradedoubler
+Requires at least: 2.8
+Tested up to: 2.9.2
+Stable tag: 1.0.0
+
+Display advertising between the introduction and post content.
+
+== Description ==
+
+Master Post Advert ia a simple plugin that display advertising between the introduction and post content.
+
+= Available languages =
+
+* english
+* polish (polski)
+
+= License =
+
+[GNU GPL version 2](http://www.gnu.org/licenses)
+
+= About =
+
+Copyright ©2010 [BBProject.net](http://www.bbproject.net)
+
+== Installation ==
+
+1. Upload the contents of the file to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Open the plugin configuration page and set up your advertisement
+
+== Screenshots ==
+
+1. Frontend - sample post
+2. Backend - advertisement settings
+
+== Changelog ==
+
+= 1.0.0 =
+* initial release
